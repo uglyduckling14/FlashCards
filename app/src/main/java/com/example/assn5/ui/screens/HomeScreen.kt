7bdc627ad743ab5bc8225299e3e4435dd7115e7b
ehauptman <esperanzahauptman@gmail.com>
2023-11-09 02:12:50 -0700

    close but no banana

--- app/src/main/java/com/example/assn5/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/assn5/ui/screens/HomeScreen.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.horizontalDrag
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -21,6 +23,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.input.pointer.positionChanged
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -28,6 +32,7 @@ import com.example.assn5.ui.components.Card
 import com.example.assn5.ui.viewmodels.HomeViewModel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 @Composable
 fun HomeScreen() {
@@ -39,74 +44,115 @@ fun HomeScreen() {
     LaunchedEffect(true){
         viewModel.setUpInitialState(0)
     }
-    viewModel.getFlashcards()
+    //viewModel.getFlashcards()
     Column(modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
 
     ) {
         val rotation = remember{ Animatable(0f) }
+        var offset = remember {
+            Animatable(100f)
+        }
         Row (modifier = Modifier
+            .offset { IntOffset(offset.value.roundToInt(), 100) }
             .fillMaxSize()
-            .offset(offsetX.dp, 0.dp)
-            .graphicsLayer {
-                rotationY = rotation.value
-            }
             .pointerInput(Unit) {
-
                 coroutineScope {
                     while (true) {
                         val pointerId = awaitPointerEventScope {
                             awaitFirstDown().id
                         }
                         rotation.stop()
+                        offset.stop()
                         awaitPointerEventScope {
                             horizontalDrag(pointerId) {
-                                launch {
-                                    if (!state.flipped) {
-                                        if(it.positionChange().x > 0 && !state.dragged){
-                                            offsetX+=it.positionChange().x
-                                        }else {
-                                            state.dragged = true
-                                            var rot =
+                                if (state.dragged && it.positionChanged()) {
+                                    offsetX += it.positionChange().x
+                                    launch {
+                                        offset.snapTo(
+                                            offsetX
+                                        )}
+                                } else
+                                    if (!state.flipped && !state.dragged) {
+                                        launch {
+                                            val rot =
                                                 clamp(rotation.value + it.positionChange().x / 5f)
                                             rotation.snapTo(
                                                 rot
                                             )
-                                            if (rot < -88f) {
-                                                state.display = state.back
-                                                state.flipped = true
-                                            }
+                                            state.flipped = true
                                         }
-                                    } else if(state.flipped){
-                                        if(it.positionChange().x < 0 && !state.dragged) {
-                                            offsetX+=it.positionChange().x
-                                        }
-                                        else {
-                                            state.dragged = true
-                                            var rot =
+                                    } else if (state.flipped && !state.dragged) {
+                                        launch {
+                                            val rot =
                                                 clamp(rotation.value + it.positionChange().x / 5f)
                                             rotation.snapTo(
                                                 rot
                                             )
-                                            if (rot > 88f) {
-                                                state.display = state.front
-                                                state.flipped = false
-                                            }
+                                            state.flipped = false
                                         }
                                     }
-                                }
                             }
-                        }
-                        launch {
-                            rotation.animateTo(0f)
-                            state.dragged = false
+                            launch {
+                                if(state.dragged){
+                                    if (offset.value < -50f && state.index != 19) {
+                                        viewModel.setUpInitialState(state.index + 1)
+                                        offset.animateTo(-50f)
+                                        offsetX = 0f
+                                        state.dragged = false
+                                    }else if(offset.value > 50f && state.index != 0){
+                                        viewModel.setUpInitialState(state.index-1)
+                                        offset.animateTo(800f)
+                                        offsetX = 0f
+                                        state.dragged = false
+                                    }
+                                }else if(state.flipped){
+                                    state.dragged = true
+                                    state.display = state.back
+                                }else if(!state.flipped){
+                                    state.display = state.front
+                                }
+                                rotation.animateTo(0f)
+
+                            }
                         }
                     }
                 }
             },
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceAround){
-            Card(prompt = state.display)
+            state.flashcards.forEachIndexed{index, card ->
+                if(index == state.index){
+                    Box(modifier = Modifier
+                        .size(200.dp, 200.dp)
+                        .offset { IntOffset(offset.value.roundToInt(), 100)}
+                        .graphicsLayer {
+                            rotationY = rotation.value
+                        }) {
+                        Card(prompt = state.display)
+                    }
+                }
+                if(index == state.index - 1 && state.index !=0){
+                    Box(modifier = Modifier
+                        .size(200.dp, 200.dp)
+                        .offset { IntOffset(offset.value.roundToInt() - 1000, 100)}
+                        .graphicsLayer {
+                            rotationY = rotation.value
+                        }) {
+                        Card(prompt = state.flashcards[index].front)
+                    }
+                }
+                if(index == state.index + 1 && state.index !=19){
+                    Box(modifier = Modifier
+                        .size(200.dp, 200.dp)
+                        .offset { IntOffset(offset.value.roundToInt() + 1000, 100)}
+                        .graphicsLayer {
+                            rotationY = rotation.value
+                        }) {
+                        Card(prompt = state.flashcards[index].front)
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/assn5/ui/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/assn5/ui/viewmodels/HomeViewModel.kt
@@ -10,7 +10,7 @@ import com.example.assn5.ui.repositories.FlashcardRepository
 
 class HomeScreenState{
     var index by mutableStateOf(0)
-    var flashcards = mutableListOf<Flashcard>()
+    var flashcards = FlashcardRepository().getFlashcards()
     var front by mutableStateOf("")
     var back by mutableStateOf("")
     var display by mutableStateOf("")
@@ -20,35 +20,12 @@ class HomeScreenState{
 class HomeViewModel(application: Application): AndroidViewModel(application) {
     val uiState = HomeScreenState()
 
-    fun getFlashcards(){
-        val repository = FlashcardRepository()
-        uiState.flashcards = mutableListOf()
-        if(uiState.index == 0){
-            uiState.front=(repository.getOne(uiState.index).front.toString())
-            uiState.back = repository.getOne(uiState.index).back.toString()
-            uiState.flashcards.add(0,repository.getOne(0))
-            uiState.flashcards.add(1, repository.getOne(1))
-            return
-        }
-        val lastIndex = repository.getFlashcards().lastIndex
-        if(uiState.index == lastIndex){
-            uiState.flashcards.add(lastIndex-1, repository.getOne(lastIndex-1))
-            uiState.front=(repository.getOne(uiState.index).front.toString())
-            uiState.back = repository.getOne(uiState.index).back.toString()
-            uiState.flashcards.add(lastIndex, repository.getOne(lastIndex))
-            return
-        }
-        uiState.flashcards.add( uiState.index-1, repository.getOne(uiState.index-1))
-        uiState.flashcards.add( uiState.index, repository.getOne(uiState.index))
-        uiState.front=(repository.getOne(uiState.index).front.toString())
-        uiState.back = repository.getOne(uiState.index).back.toString()
-        uiState.flashcards.add( uiState.index+1, repository.getOne(uiState.index+1))
-    }
     fun setUpInitialState(index:Int){
         uiState.index = index
         val repository = FlashcardRepository()
         uiState.front=(repository.getOne(uiState.index).front.toString())
         uiState.back = repository.getOne(uiState.index).back.toString()
         uiState.display = uiState.front
+        uiState.flashcards = repository.getFlashcards()
     }
 }
